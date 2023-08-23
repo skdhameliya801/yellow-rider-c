752eb513956ae1bfd20b9a60e23071623f3c2300
skdhameliya <smitdhameliya10@gmail.com>
2023-08-23 16:22:26 -0400

    uncommented server url

--- src/CONSTANTS.js
+++ src/CONSTANTS.js
@@ -1,7 +1,7 @@
 export let CONSTANTS = {
     app_name : "Yellow Trip",
-    // server_url : "https://ruby-cheerful-hummingbird.cyclic.app",
-    server_url : "http://localhost:5000",
+    server_url : "https://ruby-cheerful-hummingbird.cyclic.app",
+    // server_url : "http://localhost:5000",
     // server_url : "https://rider-api-ckvg.onrender.com",
     client_url : "yellow-trip.com"
 }
